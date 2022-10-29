3f9ed14037f043a34e5fb70222a18b00119bbca7
Thiago Castelli <thiagocastelli02@gmail.com>
2022-10-29 14:12:48 -0300

    final de la clase 18 con German

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html",
+    {
+        "nombre":"Thiago", 
+        "apellido":"Castelli",
+        
+        })
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html",
+    {
+        "nombre": nombre, 
+        "apellido": apellido,
+        
+        })
+
+def index_tres(request):
+
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1, 2, 3, 4, 5, 6, 7, 8]}
+    )
