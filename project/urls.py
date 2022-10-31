4d6ebd6b94abb9a4b2c48e06793b1a2017c2d07b
Thiago Castelli <thiagocastelli02@gmail.com>
2022-10-31 20:18:53 -0300

    logré terminar la clase 18 con el template adv, chocho

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ejemplo.views import index, index_dos, index_tres, mostrar_familiares
+from blog.views import index as blog_index
 
 
 urlpatterns = [
@@ -24,4 +25,5 @@ urlpatterns = [
     path('saludar/<nombre>/<apellido>', index_dos), 
     path('mostrar-notas/', index_tres),
     path('mi-familia/', mostrar_familiares),
+    path('blog/', blog_index),
 ]
